e7a237eb3572ff712ce6e458bb1c32564af534e2
ejazsyed <syed.ejazuddin@sap.com>
2024-02-16 23:01:35 +0530

    issu-bot

--- index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-const { run } = require('@probot/adapter-github-actions')
-const app = require("./app");
-
-run(app).catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
