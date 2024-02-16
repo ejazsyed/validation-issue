05df2f356532a2c795e08ebcf425bba5a504fdc6
ejazsyed <syed.ejazuddin@sap.com>
2024-02-16 21:17:17 +0530

    issu-bot

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Probot} app
+ */
+module.exports = (app) => {
+  // Your code here
+  app.log.info("Yay, the app was loaded!");
+
+  app.on("issues.opened", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue! we will be going to assign this to right developer",
+    });
+    const assignee = context.issue({
+      assignees: "ejazsyed",
+    })
+    context.octokit.issues.createComment(issueComment);
+    context.octokit.issues.addLabels(
+      context.issue({
+      labels: ["bug", "documentation"],
+      })
+      )
+    return context.octokit.issues.addAssignees(assignee)
+  });
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+};
